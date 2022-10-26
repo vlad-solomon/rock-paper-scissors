206405ff4ce6563cfa73e6ebe40241317a63e16e
vlad-solomon <vladsolomon720@gmail.com>
2022-10-26 16:55:47 +0300

    Fix determineGame function

--- src/components/GameOver/GameOver.jsx
+++ src/components/GameOver/GameOver.jsx
@@ -1,48 +1,10 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import GameContext from "../../context/GameContext";
 import Button from "../Button/Button";
 import "./GameOver.scss";
 
-import { options } from "../Options/Options";
-
-// const gameTable = [
-// 	["tie", "lose", "win"],
-// 	["win", "tie", "lose"],
-// 	["lose", "win", "tie"],
-// ];
-
-const gameTable = [
-	["tie", "win", "lose"],
-	["lose", "tie", "win"],
-	["win", "lose", "tie"],
-];
-
 function GameOver() {
-	const [message, setMessage] = useState("");
-	const { userPick, setUserPick, housePick, setHousePick, setIsGameOver, setIsPlaying, setScore } = useContext(GameContext);
-
-	function determineGame(user, house) {
-		const userChoice = options.findIndex((choice) => choice === user);
-		const houseChoice = options.findIndex((choice) => choice === house);
-		const result = gameTable[userChoice][houseChoice];
-		switch (result) {
-			case "win":
-				setMessage("you win");
-				setScore((prev) => prev + 1);
-				break;
-			case "lose":
-				setMessage("you lose");
-				setScore((prev) => (prev - 1 <= 0 ? 0 : prev - 1));
-				break;
-			default:
-				setMessage("it's a tie");
-				break;
-		}
-	}
-
-	useEffect(() => {
-		determineGame(userPick, housePick);
-	}, [userPick, housePick]);
+	const { message, setMessage, setUserPick, setHousePick, setIsGameOver, setIsPlaying } = useContext(GameContext);
 
 	return (
 		<div className="game-over">
@@ -54,6 +16,7 @@ function GameOver() {
 				onClick={() => {
 					setIsPlaying(false);
 					setIsGameOver(false);
+					setMessage("");
 					setUserPick("");
 					setHousePick("");
 				}}
--- src/context/GameContext.jsx
+++ src/context/GameContext.jsx
@@ -1,4 +1,11 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import { options } from "../components/Options/Options";
+
+const gameTable = [
+	["tie", "win", "lose"],
+	["lose", "tie", "win"],
+	["win", "lose", "tie"],
+];
 
 const GameContext = createContext();
 
@@ -9,8 +16,37 @@ export function GameProvider({ children }) {
 	const [isPlaying, setIsPlaying] = useState(false);
 	const [userPick, setUserPick] = useState("");
 	const [housePick, setHousePick] = useState("");
+	const [message, setMessage] = useState("");
+
+	function determineGame(user, house) {
+		const userChoice = options.findIndex((choice) => choice === user);
+		const houseChoice = options.findIndex((choice) => choice === house);
+		const result = gameTable[userChoice][houseChoice];
+		switch (result) {
+			case "win":
+				setMessage("you win");
+				setScore((prev) => prev + 1);
+				break;
+			case "lose":
+				setMessage("you lose");
+				setScore((prev) => (prev - 1 <= 0 ? 0 : prev - 1));
+				break;
+			default:
+				setMessage("it's a tie");
+				break;
+		}
+	}
+
+	useEffect(() => {
+		if (userPick && housePick) {
+			console.log(userPick, housePick);
+			determineGame(userPick, housePick);
+		}
+	}, [userPick, housePick]);
 
 	const value = {
+		message,
+		setMessage,
 		score,
 		setScore,
 		isShowing,
